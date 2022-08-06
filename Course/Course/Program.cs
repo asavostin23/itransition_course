0e7f49548e5f679227d7da4876101bfe6fa6f701
Anton Savostin <asavostin23@gmail.com>
2022-08-06 17:27:18 +0300

    Добавлена отправка email. Не деплоится

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -1,22 +1,24 @@
 using Course.Data;
 using Course.Models;
+using Course.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    + ";Password=" + builder.Configuration["Passwords:LocalDb"];
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
 builder.Services.AddIdentity<User, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultUI()
     .AddDefaultTokenProviders();
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.Services.AddTransient<Microsoft.AspNetCore.Identity.UI.Services.IEmailSender, MailRuEmailSender>();
 
 var app = builder.Build();
 
---
+++ Course/Course/Services/MailRuEmailSender.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Net;
+using System.Net.Mail;
+
+namespace Course.Services
+{
+    public class MailRuEmailSender : IEmailSender
+    {
+        protected SmtpClient SmtpClient { get; set; }
+        protected const string EmailFrom = "course.asavostin@mail.ru";
+        public MailRuEmailSender(IConfiguration configuration)
+        {
+            SmtpClient = new SmtpClient("smtp.mail.ru", 587);
+            SmtpClient.Credentials = new NetworkCredential(EmailFrom, configuration["Passwords:Email"]);
+            SmtpClient.EnableSsl = true;
+        }
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            MailMessage message = new MailMessage(new MailAddress(EmailFrom, "CourseServer"), new MailAddress(email));
+            message.Subject = subject;
+            message.Body = htmlMessage;
+            message.IsBodyHtml = true;
+            await SmtpClient.SendMailAsync(message);
+        }
+    }
+}
