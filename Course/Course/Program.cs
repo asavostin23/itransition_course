3ca31a1191a3c2d6b01cb3a059168b41d49415d9
Anton Savostin <asavostin23@gmail.com>
2022-08-07 12:47:10 +0300

    Исправлена строка подключения

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -6,8 +6,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
-    + ";Password=" + builder.Configuration["Passwords:LocalDb"];
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 
