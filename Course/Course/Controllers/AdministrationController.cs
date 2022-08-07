354f3c159e0c5e2aeb63552e60800c7f780a9865
Anton Savostin <asavostin23@gmail.com>
2022-08-07 17:25:30 +0300

    Добавлена таблица пользователей в админке

---
+++ Course/Course/Controllers/AdministrationController.cs
@@ -0,0 +1,31 @@
+﻿using Course.Data;
+using Course.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Course.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdministrationController : Controller
+    {
+        private UserManager<User> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
+        public AdministrationController(UserManager<User> userManager, RoleManager<IdentityRole> roleManager) : base()
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<UserViewModel> usersModel = _userManager.Users
+                .Select(u => new UserViewModel(u.Id, u.UserName, u.Email, u.Active, u.Language, u.Theme, false))
+                .ToList();
+            foreach (UserViewModel userModel in usersModel)
+                userModel.IsAdmin =
+                    await _userManager.IsInRoleAsync(await _userManager.FindByIdAsync(userModel.Id), "Admin");
+            return View(usersModel);
+        }
+    }
+}
+
---
+++ Course/Course/Data/Migrations/20220807122110_userActiveNotNull.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Course.Data.Migrations
+{
+    public partial class userActiveNotNull : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "Active",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true,
+                oldDefaultValue: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "Active",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: true,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldDefaultValue: true);
+        }
+    }
+}
