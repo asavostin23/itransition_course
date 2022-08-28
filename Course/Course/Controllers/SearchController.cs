a7372b1fed99ffe3244644eead84a80a19a13a8a
Anton Savostin <asavostin23@gmail.com>
2022-08-28 13:01:59 +0300

    Полнотекстовый поиск

---
+++ Course/Course/Controllers/SearchController.cs
@@ -0,0 +1,37 @@
+﻿using Course.Data;
+using Course.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGeneration.EntityFrameworkCore;
+using NuGet.Packaging;
+
+namespace Course.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public SearchController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index(string searchText)
+        {
+            List<User> userSearch = await _db.Users.Where(user => EF.Functions.FreeText(user.UserName, searchText)).ToListAsync();
+            List<Tag> tagSearch = await _db.Tags.Where(tag => EF.Functions.FreeText(tag.Name, searchText)).ToListAsync();
+            
+            List<Collection> collectionSearch =  await _db.Collections
+                .Where(collection => EF.Functions.FreeText(collection.Name, searchText)
+                || EF.Functions.FreeText(collection.Theme, searchText)
+                || EF.Functions.FreeText(collection.Description, searchText)).ToListAsync();
+            collectionSearch.AddRange(await _db.CollectionFields.Where(cf => EF.Functions.FreeText(cf.Name, searchText)).Join(_db.Collections, cf => cf.CollectionId, col => col.Id, (cf, col) => col).ToListAsync());
+
+            List<Item> itemSearch = await _db.Items.Where(item => EF.Functions.FreeText(item.Name, searchText)).ToListAsync();
+            itemSearch.AddRange(await _db.Comments.Where(comment => EF.Functions.FreeText(comment.Text, searchText)).Join(_db.Items, comment=> comment.ItemId, item => item.Id, (comment, item) => item).ToListAsync());
+            itemSearch.AddRange(await _db.StringItemFields.Where(field => EF.Functions.FreeText(field.Value, searchText)).Join(_db.Items, field => field.ItemId, item => item.Id, (field, item) => item).ToListAsync());
+            itemSearch.AddRange(await _db.TextItemFields.Where(field => EF.Functions.FreeText(field.Value, searchText)).Join(_db.Items, field => field.ItemId, item => item.Id, (field, item) => item).ToListAsync());
+            
+            return View(new SearchViewModel(itemSearch, collectionSearch, userSearch, tagSearch));
+        }
+    }
+}
---
+++ Course/Course/Models/SearchViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Course.Models
+{
+    public class SearchViewModel
+    {
+        public List<Item> Items { get; set; } = new();
+        public List<Collection> Collections { get; set; } = new();
+        public List<User> Users { get; set; } = new();
+        public List<Tag> Tags { get; set; } = new();
+        public SearchViewModel(List<Item> items, List<Collection> collections, List<User> users, List<Tag> tags)
+        {
+            Items = items;
+            Collections = collections;
+            Users = users;
+            Tags = tags;
+        }
+    }
+}
