cc53405c945020b4f3769584d9de58e3586543fe
Anton Savostin <asavostin23@gmail.com>
2022-08-29 14:48:48 +0300

    Комменты обновляются каждые 5 секунд

---
+++ Course/Course/Controllers/CommentsController.cs
@@ -0,0 +1,34 @@
+﻿using Course.Data;
+using Course.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Course.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        public CommentsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            List<Comment> itemComments = await _db.Comments.Where(comment => comment.ItemId == id).ToListAsync();
+            await _db.Users.Where(user => itemComments.Select(comment => comment.UserId).Contains(user.Id)).LoadAsync();
+            return new JsonResult(itemComments.Select(comment => new
+            {
+                Id = comment.Id,
+                UserId = comment.UserId,
+                ItemId = comment.ItemId,
+                Text = comment.Text,
+                CreatedDate = comment.CreatedDate,
+                UserName = comment.User.UserName
+            }));
+        }
+    }
+}
